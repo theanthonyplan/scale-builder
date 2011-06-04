87363959ae3c3d8e1094a46de6d5e53677c47caa
Anthony Gagliardo <theanthonyplan@gmail.com>
2011-06-04 03:54:10 -0400

    third commit

--- scaler.py
+++ scaler.py
@@ -7,13 +7,18 @@ letter_to_number = { 'a':0, 'a#':1, 'b':2, 'c':3, 'c#':4, 'd':5, 'd#':6, 'e':7,
 	'f':8, 'f#':9, 'g':10, 'g#':11}
 
 scaleMapper = { '1' : 'Major', '2' : 'Minor', '3' : 'Harmonic Minor', '4' : 'Melodic Minor',
-	'5' : 'Diminished', }
+	'5' : 'Diminished', '6' : 'Polytonal', '7' : 'Pentatonic', '8' : 'Blues',
+	'9' : 'Gospel',}
 
 scaleType = { 'Major' : [2,2,1,2,2,2,1],
 	'Minor' : [2,1,2,2,1,2,2],
 	'Harmonic Minor' : [2,1,2,2,1,3,1],
 	'Melodic Minor' : [2,1,2,2,2,2,1],
-	'Diminished' : [2,1,2,1,2,1,2,1]
+	'Diminished' : [2,1,2,1,2,1,2,1],
+	'Polytonal' : [1,2,1,2,1,2,1,2],
+	'Pentatonic' : [2,2,3,2,3],
+	'Blues'	: [3,2,1,1,3,2],
+	'Gospel' : [2,1,1,3,2,3],
 	}
 
 	
